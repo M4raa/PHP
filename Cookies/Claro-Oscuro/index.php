45bad34f56fddeeb135725f182910deacf7c612b
M4raa <davidmarpons@gmail.com>
2024-09-16 09:46:51 +0200

    Entrega_2

---
+++ Cookies/Claro-Oscuro/index.php
@@ -0,0 +1,24 @@
+<?php
+    $mode = $_GET['x'] ?? $_COOKIE["mode"] ?? 1;
+    $value = ($mode == 1) ? "light" : "dark";
+    setcookie("mode", $value, time() + (86400 * 30), "/");
+    ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styles.css" />
+    <title>Cookies-Claro/Oscuro</title>
+</head>
+<body class="<?php echo $value;?>">
+    <header>
+        <nav>
+            <ul>
+                <li><a href="?x=0">Oscuro</a></li>
+                <li><a href="?x=1">Claro</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
